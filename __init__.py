1a81c7510543717bafd79504c7d77158ade885d7
whatasame <97666463+whatasame@users.noreply.github.com>
2025-02-04 16:30:09 +0900

    feat: Enable gesture requirement for playback in browser web view

--- __init__.py
+++ __init__.py
@@ -1,7 +1,13 @@
 from aqt import gui_hooks
+from aqt.browser import Browser
+
 
 def on_editor_web_view_init(editor_web_view):
     editor_web_view.setPlaybackRequiresGesture(True)
 
+def set_browser_webview_gesture(browser: Browser):
+    browser.editor.web.setPlaybackRequiresGesture(True)
+
 gui_hooks.editor_web_view_did_init.append(on_editor_web_view_init)
+gui_hooks.browser_will_show.append(set_browser_webview_gesture)
 
